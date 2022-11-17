45ad7d59797704d49a2434effd6e28ed5ee6239d
Werbson Yuri <yuri.luz25@icloud.com>
2022-11-17 19:56:10 -0300

    ajustanto templates

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# app/assets
+Rails.application.config.assets.precompile += %w( admins_backoffice.css admins_backoffice.js
+                                                  users_backoffice.css  users_backoffice.js)
+
+# /lib/assets
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css 
+                                                  custom.js custom.css
+                                                  img.jpg undraw_profile.svg undraw_profile_1.svg undraw_profile_2.svg undraw_profile_3.svg )
+
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
