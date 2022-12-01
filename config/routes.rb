12f3fb8e1c57f361ec790f4bf184d7793877b105
Werbson Yuri <yuri.luz25@icloud.com>
2022-12-01 20:04:25 -0300

    ajustando rotas admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  
   namespace :users_backoffice do
     get 'welcome/index'
   end
+  
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins , only: [:index]  # Administrators
   end
+  
   devise_for :users
   devise_for :admins
-  get 'welcome/index'
-  get 'inicio', to: 'welcome#index'
-  root to: 'welcome#index'
+  
+  get 'inicio', to: 'site/welcome#index'
+  
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
