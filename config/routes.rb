d6d7cb50aa03741c50260a4462954bd5b15d92e3
Werbson Yuri <yuri.luz25@icloud.com>
2023-01-18 13:52:16 -0300

    ajustando crud do edit e update admin

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins , only: [:index]  # Administrators
+    resources :admins , only: [:index, :edit, :update]  # Administrators
   end
   
   devise_for :users
